d37484c01148e6d2555a306abdcba4aaf9b90df2
Marisol Leon <marisol.leon@uabc.edu.mx>
2021-08-31 19:40:57 -0700

    Añadi la tarea de nombre y edad

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre, edad;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            edad = Console.ReadLine();
+            Console.WriteLine("Te llamas " + nombre + "y tienes " + edad + "años");
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
